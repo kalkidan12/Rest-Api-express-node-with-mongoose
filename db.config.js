c805a71560ddf633b5322f67c19070be0e9e5454
kalkidan12 <kalget12@gmail.com>
2022-05-20 16:01:33 +0300

    co

--- db.config.js
+++ db.config.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 module.exports = () => {
-  mongoose.connect(
-    "mongodb+srv://daniel:hidaniel@cluster0.giaa7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    (err) => {
-      if (err) {
-        console.log("error connecting to db" + err);
-      } else {
-        console.log("connect to db");
-      }
-    }
-  );
+	mongoose.connect(
+		"mongodb+srv://kalkidan:<kalkidan>@internship.9s8dq.mongodb.net/?retryWrites=true&w=majority",
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+		(err) => {
+			if (err) {
+				console.log("error connecting to db" + err);
+			} else {
+				console.log("connect to db");
+			}
+		},
+	);
 };
